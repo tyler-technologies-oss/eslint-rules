b8dae169432094984fa5ef155771751021a8e788
Paul Lapczynski <paul.lapczynski@tylertech.com>
2021-05-25 11:53:24 -0400

    added deploy package scripts

---
+++ build-scripts/publish.ps1
@@ -0,0 +1,37 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$VersionNumber
+)
+
+$ErrorActionPreference = "Stop";
+
+$packageJson = "./build/_publish/package.json";
+$release = $false;
+
+# import common functions script from the same folder
+$thisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. ($thisScript + '/CommonFunctions.ps1')
+
+Get-Tags;
+
+Push-Location
+Set-Location $(Join-Path $PSScriptRoot ..)
+
+Write-Host "Install npm packages..."
+Invoke-Expression "npm install"
+
+Write-Host "Building eslint-plugin"
+Invoke-Expression "cd packages/eslint-plugin"
+Invoke-Expression "npm run build"
+Invoke-Expression "npm run publish"
+
+$VersionNumber = Update-Version-Number-PackageJson $packageJson $VersionNumber $release;
+Write-Host "========== Building Version: $VersionNumber =========="
+Write-Host "##teamcity[buildNumber '$($VersionNumber)']"
+
+Write-Host "Publishing eslint-plugin"
+npm publish --registry "$env:ARTIFACTORY_NPM_REGISTRY" ./publish
+
+Pop-Location
+
+Add-Build-Tag "v$VersionNumber"
---
+++ packages/eslint-plugin-angular/gulpfile.ts
@@ -0,0 +1,49 @@
+import { task } from 'gulp';
+import { resolve, join } from 'path';
+import fs from 'fs';
+import ncp from 'ncp'
+
+const ROOT = resolve(__dirname, './');
+const OUTPUT_DIR = join(ROOT, 'publish');
+const PACKAGE_DIST = join(ROOT, 'dist');
+const PACKAGE_DIST_PUBLISH = join(OUTPUT_DIR, 'dist');
+
+task('publish', async done => {
+    const copyBuildToPublishFolder = new Promise(resolve => {
+        ncp(PACKAGE_DIST, PACKAGE_DIST_PUBLISH, () => {
+            resolve(true);
+        });
+    });
+
+    const copyPackageJson = new Promise(resolve => {
+        ncp(join(ROOT, 'package.json'), join(OUTPUT_DIR, 'package.json'), () => {
+            resolve(true);
+        });
+    });
+
+    const createPublishFolder = new Promise(resolve => {
+        fs.mkdir(OUTPUT_DIR, { recursive: true }, () => {
+            resolve(true);
+        });
+    });
+
+    const createPublishDistFolder = new Promise(resolve => {
+        fs.mkdir(PACKAGE_DIST_PUBLISH, () => {
+            resolve(true);
+        });
+    });
+
+    const copyTypings = new Promise(resolve => {
+        ncp(join(ROOT, 'typings'), join(OUTPUT_DIR, 'typings'), () => {
+            resolve(true)
+        });
+    });
+
+    await createPublishFolder;
+    await copyBuildToPublishFolder;
+    await createPublishDistFolder;
+    await copyPackageJson;
+    await copyTypings;
+
+    done();
+});
