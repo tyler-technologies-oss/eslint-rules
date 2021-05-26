3c83cf114f946f6f9fda5f81afa1dc39dbec64d9
Paul Lapczynski <paul.lapczynski@tylertech.com>
2021-05-26 09:16:11 -0400

    wired up all packages to create npm packages

--- build-scripts/publish.ps1
+++ build-scripts/publish.ps1
@@ -1,7 +1,7 @@
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$VersionNumber,
-    [string]$Tag
+  [Parameter(Mandatory = $true)]
+  [string]$VersionNumber,
+  [string]$Tag
 )
 
 $BranchName = $Tag.Substring(0, $Tag.IndexOf('-v'))
@@ -27,20 +27,33 @@ Set-Location $(Join-Path $PSScriptRoot ..)
 
 $packages = Get-ChildItem "packages";
 Write-Host "======Checking for package changes========"
-Write-Host "$packages"
 foreach ($package in $packages) {
   $item = $package.name
-    $changes = Invoke-Expression "git diff --name-only origin/master HEAD" | Where-Object { $_ -like "packages/$item/*" };
-    Write-Host $changes
-    $hasPackageChanged = $changes.count -gt 0;
+  $changes = Invoke-Expression "git diff --name-only origin/master HEAD" | Where-Object { $_ -like "packages/$item/*" };
+  $hasPackageChanged = $changes.count -gt 0;
+  if ($hasPackageChanged) {
+    Write-Output "The Package $item has changed"
+    Write-Output "Updating $item npm package..."
+    Invoke-Expression "cd $item"
+    Write-Host "Install npm packages..."
+    Invoke-Expression "npm install"
+    Invoke-Expression "npm run build:publish"
 
-    if ($hasPackageChanged) {
-        Write-Output "The Package $item has changed"
-        Write-Output "Updating $item npm package..."
-    }
+    $VersionNumber = Update-Version-Number-PackageJson $packageJson $VersionNumber $release;
+    Write-Host "========== Building Version: $VersionNumber =========="
+    Write-Host "##github[buildNumber '$($VersionNumber)']"
+
+    Write-Host "Publishing" + $item.Substrin("packages/")
+    npm publish --registry "$env:ARTIFACTORY_NPM_REGISTRY" ./publish
+    Invoke-Expression "cd .."
+  }
 }
 
+Pop-Location
 
+if ($Tag) {
+  Add-Build-Tag "$Tag"
+}
 
 # Write-Host "Install npm packages..."
 # Invoke-Expression "npm install"
