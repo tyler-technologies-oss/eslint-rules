9aa57a44d9f2128b303751077fdfad45ac8acbad
Nichols, Kieran <kieran.nichols@tylertech.com>
2025-06-18 14:07:00 -0400

    chore: remove shared test config

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -2,7 +2,6 @@ import rules from './rules';
 import createBaseConfig from './configs/base';
 import createRecommendedConfig from './configs/recommended';
 import createStrictConfig from './configs/strict';
-import createTestRecommendedConfig from './configs/test';
 import createTypeScriptRecommendedConfig from './configs/typescript';
 import { TSESLint } from '@typescript-eslint/utils';
 import { name, version } from '../package.json';
@@ -16,12 +15,10 @@ const base = createBaseConfig();
 const recommended = createRecommendedConfig(plugin);
 const strict = createStrictConfig(plugin);
 const tsRecommended = createTypeScriptRecommendedConfig(plugin);
-const testRecommended = createTestRecommendedConfig(plugin);
 const configs = {
   base,
   recommended,
   strict,
-  testRecommended,
 } satisfies Record<string, TSESLint.FlatConfig.Config | TSESLint.FlatConfig.ConfigArray>;
 
 export default {
@@ -29,4 +26,4 @@ export default {
   configs,
 } satisfies TSESLint.FlatConfig.Plugin;
 
-export { base, recommended, strict, tsRecommended, testRecommended };
+export { base, recommended, strict, tsRecommended };
