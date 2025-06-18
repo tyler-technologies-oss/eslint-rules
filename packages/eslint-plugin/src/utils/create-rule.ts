5fe16106679e01ae19862ff107cc2a78ac4c6a87
Nichols, Kieran <kieran.nichols@tylertech.com>
2025-06-18 14:06:25 -0400

    chore: add rule docs and fix rule URLs

--- packages/eslint-plugin/src/utils/create-rule.ts
+++ packages/eslint-plugin/src/utils/create-rule.ts
@@ -1,5 +1,6 @@
 import { ESLintUtils } from '@typescript-eslint/utils';
 
 export const createRule = ESLintUtils.RuleCreator(
-  () => 'https://github.com/tyler-technologies-oss/eslint-rules/tree/main/packages/eslint-plugin/src/rules',
+  ruleName =>
+    `https://github.com/tyler-technologies-oss/eslint-rules/tree/main/packages/eslint-plugin/docs/rules/${ruleName}.md`,
 );
