f5a9f4d4dcb8817750110a65ae5824f270bf20ba
Nichols, Kieran <kieran.nichols@tylertech.com>
2025-06-20 13:44:17 -0400

    chore: add missing strict rule

--- packages/eslint-plugin/src/configs/strict.ts
+++ packages/eslint-plugin/src/configs/strict.ts
@@ -20,6 +20,7 @@ export default (plugin: FlatConfig.Plugin): FlatConfig.ConfigArray => [
       'prefer-object-has-own': 'error',
       'arrow-body-style': ['warn', 'as-needed'],
       'no-undef': 'error',
+      camelcase: 'error',
     },
   },
 ];
