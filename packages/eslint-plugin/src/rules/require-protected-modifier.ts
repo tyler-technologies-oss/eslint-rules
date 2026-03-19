441b97120d5f1893044ebee35a0b08a288e3979a
Paul Lapczynski <paul.lapczynski@tylertech.com>
2026-03-19 14:56:57 -0400

    chore: make protected optional

--- packages/eslint-plugin/src/rules/require-protected-modifier.ts
+++ packages/eslint-plugin/src/rules/require-protected-modifier.ts
@@ -2,10 +2,10 @@ import { createRule } from '../utils/create-rule';
 import { AST_TOKEN_TYPES, TSESTree } from '@typescript-eslint/utils';
 
 type Options = string[];
-type MessageIds = 'requirePrivateModifier';
+type MessageIds = 'requireProtectedModifier';
 
 const ALLOWED_MEMBERS = ['constructor'];
-export const RULE_NAME = 'require-private-modifier';
+export const RULE_NAME = 'require-protected-modifier';
 
 export default createRule<Options, MessageIds>({
   name: RULE_NAME,
@@ -13,11 +13,11 @@ export default createRule<Options, MessageIds>({
     type: 'suggestion',
     docs: {
       description:
-        'Requires properties or methods that start with an underscore to be marked with a private or protected modifier.',
+        'Requires properties or methods that start with an underscore to be marked with a protected or private modifier.',
     },
     schema: [],
     messages: {
-      requirePrivateModifier:
+      requireProtectedModifier:
         "'{{nodeInfo}}' must be marked as private or protected if it starts with an underscore",
     },
     fixable: 'code',
@@ -40,7 +40,7 @@ export default createRule<Options, MessageIds>({
       ) {
         context.report({
           data: { type: node.type, nodeInfo: propertyName },
-          messageId: 'requirePrivateModifier',
+          messageId: 'requireProtectedModifier',
           node: node,
           fix: function (fixer) {
             const tokens = sourceCode.getTokens(node);
@@ -48,9 +48,9 @@ export default createRule<Options, MessageIds>({
               t => t.type === AST_TOKEN_TYPES.Keyword && t.value === 'public',
             );
             if (publicToken) {
-              return fixer.replaceText(publicToken, 'private');
+              return fixer.replaceText(publicToken, 'protected');
             }
-            return fixer.insertTextBefore(node, 'private ');
+            return fixer.insertTextBefore(node, 'protected ');
           },
         });
       }
